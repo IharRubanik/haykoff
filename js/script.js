b7112f13477ea69fa2a45c4283a5b8234a261e60
IharRubanik <igor.rubanik@gmail.com>
2022-07-12 00:08:12 +0400

    end

--- js/script.js
+++ js/script.js
@@ -2,58 +2,85 @@ window.addEventListener("load", function () {
   let picturesArrey = document.querySelectorAll(".pictures");
   let slides = document.querySelectorAll(".slide");
   let slideIndex = 0;
+  let body = document.querySelector("body");
+  let openMenu = document.querySelector("#open-menu");
+  let closeMenu = document.querySelector("#close-menu");
+  let menuMobile = document.querySelector(".menu__mobile");
 
-
-  setInterval(() => {
-    picturesArrey[5].classList.add("active");
-  }, 200);
-  setInterval(() => {
-    picturesArrey[11].classList.add("active");
-  }, 400);
-  setInterval(() => {
-    picturesArrey[3].classList.add("active");
-  }, 600);
-  setInterval(() => {
-    picturesArrey[12].classList.add("active");
-  }, 800);
-  setInterval(() => {
-    picturesArrey[9].classList.add("active");
-  }, 1000);
-  setInterval(() => {
-    picturesArrey[2].classList.add("active");
-  }, 1200);
-  setInterval(() => {
-    picturesArrey[0].classList.add("active");
-  }, 1400);
-  setInterval(() => {
-    picturesArrey[6].classList.add("active");
-  }, 1600);
-  setInterval(() => {
-    picturesArrey[14].classList.add("active");
-  }, 1800);
-  setInterval(() => {
-    picturesArrey[10].classList.add("active");
-  }, 2000);
-  setInterval(() => {
-    picturesArrey[8].classList.add("active");
-  }, 2200);
-  setInterval(() => {
-    picturesArrey[1].classList.add("active");
-  }, 2400);
-  setInterval(() => {
-    picturesArrey[7].classList.add("active");
-  }, 2600);
-  setInterval(() => {
-    picturesArrey[13].classList.add("active");
-  }, 2800);
-  setInterval(() => {
-    picturesArrey[4].classList.add("active");
-  }, 3000);
-  setInterval(() => {
-    picturesArrey[15].classList.add("active");
-  }, 3200);
-
-
+  openMenu.onclick = function() {
+    body.style.overflow = "hidden";
+    menuMobile.style.zIndex = 99;
+    menuMobile.classList.add("active")
+  }
+  closeMenu.onclick = function() {
+    body.style.overflow = "visible";
+    menuMobile.classList.remove("active")
+    setTimeout(() => {
+     menuMobile.style.zIndex = -1;
+    }, 100);
+  }
+  function showImages() {
+    setTimeout(() => {
+      picturesArrey[5].classList.add("active");
+    }, 200);
+    setTimeout(() => {
+      picturesArrey[11].classList.add("active");
+    }, 400);
+    setTimeout(() => {
+      picturesArrey[3].classList.add("active");
+    }, 600);
+    setTimeout(() => {
+      picturesArrey[12].classList.add("active");
+    }, 800);
+    setTimeout(() => {
+      picturesArrey[9].classList.add("active");
+    }, 1000);
+    setTimeout(() => {
+      picturesArrey[2].classList.add("active");
+    }, 1200);
+    setTimeout(() => {
+      picturesArrey[0].classList.add("active");
+    }, 1400);
+    setTimeout(() => {
+      picturesArrey[6].classList.add("active");
+    }, 1600);
+    setTimeout(() => {
+      picturesArrey[14].classList.add("active");
+    }, 1800);
+    setTimeout(() => {
+      picturesArrey[10].classList.add("active");
+    }, 2000);
+    setTimeout(() => {
+      picturesArrey[8].classList.add("active");
+    }, 2200);
+    setTimeout(() => {
+      picturesArrey[1].classList.add("active");
+    }, 2400);
+    setTimeout(() => {
+      picturesArrey[7].classList.add("active");
+    }, 2600);
+    setTimeout(() => {
+      picturesArrey[13].classList.add("active");
+    }, 2800);
+    setTimeout(() => {
+      picturesArrey[4].classList.add("active");
+    }, 3000);
+    setTimeout(() => {
+      picturesArrey[15].classList.add("active");
+    }, 3200);
+    setTimeout(() => {
+      hidenImages ();
+    }, 3400);
+  }
+showImages()
+ function hidenImages () {
+  for (i = 0; i < picturesArrey.length; i++) {
+    picturesArrey[i].classList.remove('active')
+  }
+  setTimeout(() => {
+    showImages()
+  },200)
+ }
 
 //   Slider
   function sliderAbout() {
@@ -71,12 +98,11 @@ window.addEventListener("load", function () {
     sliderAbout();
   }, 1500);
 
-
 // Scroll
   window.onscroll = function (e) {
-    console.log(window.scrollY);
+
   if(window.scrollY >= 120) {
-    console.log('active')
+   
     document.querySelector('.photo').classList.add('active')
   }
   else{
@@ -84,22 +110,4 @@ window.addEventListener("load", function () {
   }
 };
 
-
-
-
 });
-
-
-
-// function setDellay(currentIndex) {
-//     setTimeout(function () {
-//       picturesArrey[currentIndex].classList.add("active");
-//     }, dellay * currentIndex);
-//   }
-//   for (
-//     let currentIndex = 0;
-//     currentIndex < picturesArrey.length;
-//     currentIndex++
-//   ) {
-//     setDellay(currentIndex);
-//   }
